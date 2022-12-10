d0a36c21632e2edd91b27f2c5a9c1098d3235511
Oliver Forral <intrepion@gmail.com>
2022-12-10 09:41:22 -0700

    Fixing issues for putting on Digital Ocean.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+    "Hello, World!"
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok)))
-        .bind(("127.0.0.1", 8080))?
+    HttpServer::new(|| App::new().route("/", web::get().to(hello)))
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
